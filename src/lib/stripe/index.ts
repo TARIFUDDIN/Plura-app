9309f7541810cc850dbbd4f5bb0d2e8ae1b63b25
TARIFUDDIN <rafiuddin.tarif@gmail.com>
2025-07-25 22:22:06 +0530

    updated stripe

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -1,10 +1,9 @@
-// lib/stripe/index.ts
-import Stripe from 'stripe'
+import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2025-06-30.basil', // Use the version your types expect
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2023-10-16",
   appInfo: {
-    name: 'Plura App',
-    version: '0.1.0',
+    name: "Zendo App",
+    version: "0.1.0",
   },
-})
+});
